adebacdaca965cdd5f26974fe60c9cd21c65fc00
PIYATHIDA CHOOJAN <61010656@kmitl.ac.th>
2019-07-21 08:57:23 +0700

    Bg-Done

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react"
+import './App.css'
+import Bg from "../Bg/Bg.js"
+
+
+function App() {
+  return (
+    <Bg/>
+  );
+}
+
+export default App;
---
+++ src/components/Bg/Bg.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react"
+import "../Bg/Bg.css"
+
+class Bg extends Component{
+    render(){
+        return(
+            <div className="WindowPage">
+                <nav className="NevBar"></nav>
+                <div className="HalfCircle"></div>
+            </div>
+             
+        );
+    }
+}
+export default Bg;
